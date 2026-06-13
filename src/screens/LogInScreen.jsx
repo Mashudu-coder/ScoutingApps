208e3bc27b50166ffa6a9517930bbccd0162b542
Mashudu-coder <rathogwalucky06@gmail.com>
2026-06-13 02:00:46 +0200

    Remove unused Button import and navigation variable from LogInScreen component

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,11 +1,11 @@
-import {Text, Button, View } from 'react-native';
+import {Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { use } from 'react';
 
 export default function LogInScreen(){
 
-    const navigation = useNavigation();
+    
     return (
         <View>
             <Text>Welcome</Text>
