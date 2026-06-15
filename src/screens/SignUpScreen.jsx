342c73611b201444d3d92aa06e031e30e66f471b
Mashudu-coder <rathogwalucky06@gmail.com>
2026-06-15 08:12:40 +0200

    Add first name and last name inputs to SignUpScreen; implement layout adjustments

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Text, View, Pressable, ScrollView, StyleSheet, Platform, Image} from 'react-native';
+import {Text, View, Pressable, ScrollView, StyleSheet, Platform, Image,} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import ThemedTextInput from '../components/ThemedTextInput';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import appLogo from '../../assets/Images/Untitled design-Photoroom.png';
+import {useState} from 'react'
+
 
 
 export default function SignUp(){
@@ -12,32 +14,77 @@ export default function SignUp(){
      const navigation = useNavigation();
      const insets = useSafeAreaInsets();
 
+     const [firstName, setFirstName] = useState('');
+     const [lastName, setLastName ] = useState('');
+
     return (
 
-    <KeyboardAwareScrollView
-            style={{ flex: 1 }}
-            contentContainerStyle={{ flexGrow: 1 }}
+        <View style={{flex: 1}}>
+
+            <View style={styles.pageHeading}>
+                 <Image source={appLogo} style={styles.styleLogo} />
+                 <Text style={styles.logoText}>RIME SCOUT</Text>
+            </View>
+           
+
+          <KeyboardAwareScrollView
+            style={{ flex: 1 }, styles.container}
+            contentContainerStyle={{ 
+                flexGrow: 1, 
+                paddingTop: insets.top, 
+                paddingBottom: insets.bottom + 16 }}
             enableOnAndroid={true}
             extraScrollHeight={Platform.OS === 'ios' ? 0 : 20}
             keyboardShouldPersistTaps="handled"
             showsVerticalScrollIndicator={false}
         >
-           <ScrollView
-                    contentContainerStyle={[
-                {
-                    paddingTop: insets.top, 
-                    paddingBottom: insets.bottom + 16 }]}>
 
-                        <Image source={appLogo} style={styles.styleLogo} />
+            <Text style={{marginBottom: 5, left: 6, fontWeight: 'semibold'}}>First Name</Text>
+            <ThemedTextInput
+              placeholder='Enter your first name'
+              autoCapitalise = 'none'
+              value={firstName}
+              onChangeText={(text) => setFirstName(text)}
+            />
 
+               <Text style={{marginBottom: 5, left: 6, fontWeight: 'semibold'}}>Last Name</Text>
+            <ThemedTextInput
+              placeholder='Enter your last name'
+              autoCapitalise = 'none'
+              value={lastName}
+              onChangeText={(text) => setLastName(text)}
+            /> 
 
-            </ScrollView>
-           
-    </KeyboardAwareScrollView>
-
+           </KeyboardAwareScrollView>
+             </View>
     )
 }
 
 const styles= StyleSheet.create({
+    styleLogo:{
+     width: 180,
+    height: 150,
+    resizeMode: 'contain',
+    alignSelf: 'flex-start', 
+    marginLeft: -50,          
+    marginBottom: 20,
+               
+    },
+
+    pageHeading: {
+        flexDirection: 'row',
+    },
+     
+    logoText:{
+    alignSelf: 'flex-start', 
+    marginLeft: -69,          
+    marginTop: 50,
+    fontWeight: 'bold',
+    fontSize: 18         
+    },
+    container:{
+        left: 14,
+        
+    }
 
 })
---
+++ src/screens/SplashScreen.jsx
@@ -0,0 +1,16 @@
+import{View, Image, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native/types_generated/index';
+
+
+export default function SplashScreen(){
+
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
