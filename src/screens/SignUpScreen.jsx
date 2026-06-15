5282affd72193cbd5bca3e9b35fb02407426df72
Mashudu-coder <rathogwalucky06@gmail.com>
2026-06-15 04:35:33 +0200

    Add app logo to SignUpScreen and update image import

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Text, View, Pressable, ScrollView, StyleSheet, Platform} from 'react-native';
+import {Text, View, Pressable, ScrollView, StyleSheet, Platform, Image} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import ThemedTextInput from '../components/ThemedTextInput';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import appLogo from '../../assets/Images/Untitled design-Photoroom.png';
 
 
 export default function SignUp(){
@@ -27,8 +28,10 @@ export default function SignUp(){
                     paddingTop: insets.top, 
                     paddingBottom: insets.bottom + 16 }]}>
 
-                        <Image source={} />
-                </ScrollView>
+                        <Image source={appLogo} style={styles.styleLogo} />
+
+
+            </ScrollView>
            
     </KeyboardAwareScrollView>
 
